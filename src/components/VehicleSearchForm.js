b87070f48b28d6ff4c59cc1b0ee0deb86aff2976
Burak Kocak <burakkocak884@hotmail.com>
2019-11-05 20:36:45 -0500

    code clean up

--- src/components/VehicleSearchForm.js
+++ src/components/VehicleSearchForm.js
@@ -3,40 +3,28 @@ import { connect} from 'react-redux';
 import {Form, Button} from 'semantic-ui-react';
 import {findVehicles} from '../action/Vehicles'
 class VehicleSearchForm extends Component{
-	constructor(props){
-super(props)
+					constructor(props){
+					super(props)
 
-this.state={
-	year: 0,
-	make: ''
-}
-}
+				this.state={
+					year: 0,
+					make: ''
+					}
+					}
 
 		
-		handleChange = event =>{
-this.setState({
+				handleChange = event =>{
+					this.setState({[event.target.name]: event.target.value})
+					}
 
-	[event.target.name]: event.target.value
-})
-
-		}
-
-		handleSubmit = event =>{
-		
-			
-			event.preventDefault()
-			// this.state.year = parseInt(this.state.year)
-		
-			this.props.findVehicles(this.state)
-			this.setState({
-				year: 0,
-				make:''
-				
-			})
-
-			
-
-		}
+				handleSubmit = event =>{
+					event.preventDefault()
+					this.props.findVehicles(this.state)
+					this.setState({
+						year: 0,
+						make:''
+						})
+					}
 	
 
 
@@ -45,51 +33,25 @@ this.setState({
 		
 
 	render(){
-		
-		// console.log(this.props)
+			return (
+				 <Form >
+					<Form.Field>
+						<label>Year</label>
+						<input   type="number" onChange={this.handleChange} name='year' placeholder="1900-2020"/>
+					</Form.Field>
 
-		
-		return (
-			
-            
-            <Form >
-			
-			
-			
-				<Form.Field>
-					<label>Year</label>
-					<input   type="number" onChange={this.handleChange} name='year' placeholder="1900-2020"/>
-				</Form.Field>
+					<Form.Field>
+						<label>Manufacturer Name</label>
+						<input   type="text" onChange={this.handleChange} name='make' placeholder="Make"/>
+					</Form.Field>
 
-				<Form.Field>
-					<label>Manufacturer Name</label>
-					<input   type="text" onChange={this.handleChange} name='make' placeholder="Make"/>
-				</Form.Field>
-
-	             <Form.Field>
-					<Button type="submit" onClick={this.handleSubmit}>Search </Button>
-				 </Form.Field>
-			</Form>
-
-
-			
-
-
-
-			)
-
-
-
-
-
-}
+					<Form.Field>
+						<Button type="submit" onClick={this.handleSubmit}>Search </Button>
+					</Form.Field>
+				</Form>
+					)}
 }
 
-
-// const mapDispatchToProps = dispatch => ({
-  
-//   findVehicles: searchVehicle => dispatch({type: "FIND_VEHICLES",searchVehicle})
-// })
 export default connect(null,{findVehicles})(VehicleSearchForm);
 
 
