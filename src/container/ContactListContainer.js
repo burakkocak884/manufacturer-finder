cc16a84f2ebf349a53b1064240375af030dddd03
Burak Kocak <burakkocak884@gmail.com>
2022-06-15 09:01:36 -0400

    UI adjustments

--- src/container/ContactListContainer.js
+++ src/container/ContactListContainer.js
@@ -5,25 +5,24 @@ import {deleteContact} from '../action/Vehicles';
 class ContactListContainer extends Component {
 
  	render(){
-		// console.log("contact list=",this.props.contactList)
-
+		
  			if(this.props.contactList && this.props.contactList.length > 0){
 				let sortedContact= this.props.contactList.sort(function(a,b){
 				return a.Make_Name < b.Make_Name ? -1 : a.Make_Name >b.Make_Name ? 1 : 0;
 			})
 					return(
 						<div>
-						<h1>Contact List:</h1>
+								<h1>Contact List:</h1>
 								<div class = 'row'>
 								  {sortedContact.map(c =>(
-									<div class = 'column'><div class = 'card'><h1>ID# {c.Make_ID}</h1>  <h3>{c.Make_Name}</h3><button onClick={()=> this.props.deleteContact(c.Make_ID)}>X</button></div></div>
+									<div class = 'column'><div class = 'card'><h5>{c.Make_Name.substring(0, 24)}...</h5><button onClick={()=> this.props.deleteContact(c.Make_ID)}>X</button></div></div>
 										))}
 							  
 							  	</div>
 						</div>
 							)
 			}else{
-				return(<div><p>Contact List is Empty</p></div>)
+					return(<div><p>Contact List is Empty</p></div>)
 	}
 }
 }
--- src/container/FoundManufacturerContainer.js
+++ src/container/FoundManufacturerContainer.js
@@ -6,33 +6,118 @@ class FoundManufacturerContainer extends Component {
 
 
 	render(){
-		 //console.log("props",this.props)
+		 
        const{foundManufacturers, searchCriteria} = this.props
 	   let foundIt;
 	   let maker;
-	   let year;
+	   let firstSet;
+	   let secondSet;
+	   let thirdSet;
+	   let groupSizeMod;
 
   		 if (foundManufacturers && foundManufacturers.length > 0){
    		let sortedM = this.props.foundManufacturers.sort(function(a,b){
-			
 			return a.Model_Name < b.Model_Name ? -1 : a.Model_Name >b.Model_Name ? 1 : 0;
 			})
 		maker = foundManufacturers[0].Make_Name;
-		year = searchCriteria.year;
-		//year = foundManufacturers[0].year;
-
-		foundIt = 	<div>
-     					<h2 class="cars-found'">We found <span className='result-length'>{foundManufacturers.length}</span> products Manufactured or Sold by <span className='result-length'>{maker}</span> in <span className='result-length'>{year}</span> in the U.S.</h2>
-						   	 <ol>
-						   	{sortedM.map((v,index) => (
-						   			<div key={v.id} >
-						   			
-							   			<li id={index}>{v.Model_Name}</li>
-											
+		groupSizeMod = sortedM.length % 14;
+		let numberOfGroups = (sortedM.length - groupSizeMod) / 14;
+			
+		
+			foundIt = <div>	 
+			<h2 class='cars-found'>We found <span className='result-length'>{foundManufacturers.length}</span> products Manufactured and Sold by <span className='result-length'>{maker}</span> in the U.S.</h2>
+			<div>
+			<div className = 'found-model-container'>
+								<div className = 'row'><div className = 'column'>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(0,14).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
 									</div>
-											))}
-						   	</ol>
-   					</div>
+									<br></br>
+									
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(14,28).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(28,42).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(42,54).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(54,68).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(68,82).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(82,96).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+									<br></br>
+									<div className = 'card'>
+												<ul>
+													{sortedM.slice(96,110).map((v,index) => (
+															<div key={v.id} >
+																<li id={index}>{v.Model_Name}</li>
+															</div>
+																	))}
+												</ul>
+									</div>
+
+								</div></div>
+								</div></div>
+			</div>
+			
+						
+													
+						
+				
 
    			}else{
 			foundIt = <p>Type a year and name of a manufacturer  in the form to list if any products offered.</p>
@@ -45,8 +130,11 @@ class FoundManufacturerContainer extends Component {
 				
 				</div>
 				 	<VehicleSearchForm />
-				{foundIt}
-			</div>
+
+					
+							{foundIt}
+					</div>
+				
 			)}
 }	
 
